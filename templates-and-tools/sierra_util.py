d0764d1f7567b3e19a4ca551c13531ae92e885cf
pcolar <dave@pcolar.com>
2020-03-27 10:31:14 -0400

    additions for deploy_code powershell script

--- templates-and-tools/sierra_util.py
+++ templates-and-tools/sierra_util.py
@@ -8,7 +8,19 @@ def get_token():
     url = "https://catalog.chapelhillpubliclibrary.org/iii/sierra-api/v5/token"
 
     # Get the API key from secrets file
-    header = {"Authorization": "Basic " + str(secrets.sierra_api_2), "Content-Type": "application/x-www-form-urlencoded"}
+    header = {"Authorization": "Basic " + str(secrets.sierra_api), "Content-Type": "application/x-www-form-urlencoded"}
+    response = requests.post(url, headers=header)
+    if response.status_code != 200:
+        print(f'get Sierra Token failed with code {response.status_code} ')
+        exit(1)
+    json_response = json.loads(response.text)
+    return json_response["access_token"]
+
+# Function to get the API token
+def get_sierra_token():
+    url = "https://catalog.chapelhillpubliclibrary.org/iii/sierra-api/v5/token"
+    # Get the API key from secrets.py
+    header = {"Authorization": "Basic " + str(secrets.sierra_api), "Content-Type": "application/x-www-form-urlencoded"}
     response = requests.post(url, headers=header)
     if response.status_code != 200:
         print(f'get Sierra Token failed with code {response.status_code} ')
