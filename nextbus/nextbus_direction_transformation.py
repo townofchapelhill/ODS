1065853d4a27d9142e6e692faddf46043b752a44
pcolar <dave@pcolar.com>
2020-06-09 11:10:06 -0400

    Add code from the stand-alone repo and start mapping process to incorporate transformations

---
+++ nextbus/nextbus_direction_transformation.py
@@ -0,0 +1,24 @@
+### Dictionary to translate the abbreviated nextbus direction to an expanded readable direction
+
+direction_lookup = {
+    "tomlkblvd"	: "To Martin Luther King Jr Blvd",
+    "tonrthside": "To Northside",
+    "chatpr" : "To Chatham Park and Ride",
+    "tounccampu" : "To UNC Campus",
+    "2colosage" : "To Colony Lake - Sage Rd",
+    "2unc" : "To UNC Hospital",
+    "tofamily" : "To Family Practice Building",
+    "tojonesfpr" : "To Jones Ferry Park and Ride",
+    "tocarrboro" : "To Carrboro Plaza",
+    "tounccampus" : "To UNC Campus",
+    "2pittsboro" : "To Pittsboro St",
+    "2eastowne" : "To Eastowne",
+    "2smitbpw" : "To Smith Level and BPW Club",
+    "2unch" : "To UNC Hospital",
+    "2greenbrg" : "To Greenbridge",
+    "tocolonywd" : "To Colony Woods",
+    "tomcdougle" : "To McDougle School",
+    "2friday" : "To Friday Center Parking Lot",
+    "tovarsity" : "To Varsity Theater",
+    "2booker" : "To Booker Creek"
+}
