e5b42a490789a69114b5276eb9a446be1c0e98b6
pcolar <dave@pcolar.com>
2020-06-25 09:05:49 -0400

    redact file paths, refactor import statements to support logging

--- analytics/dailysessions.py
+++ analytics/dailysessions.py
@@ -1,20 +1,16 @@
-# Create file to log exception handling
-log_file = open("dailysessionserror.txt", "a")
-
 # Import modules/libraries
-try: 
-    from apiclient.discovery import build
-    from oauth2client.service_account import ServiceAccountCredentials
-    import secrets
-    import traceback
-    import datetime
-    import csv
-    import os
-    import filename_secrets
-except:
-    print("Could not import all libraries.")
-    log_file.write("Library Import Failed")
-    log_file.write(traceback.format_exc())
+import secrets
+import traceback
+import datetime
+import csv
+import os
+import filename_secrets
+from apiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Create file to log exception handling
+logFilename = os.path.join(filename_secrets.logfilesDirectory, "dailysessionserror.txt")
+log_file = open(logFilename, "a")
 
 # Authentication 
 SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
